9614bd56e1a6061eb918dbee3c829b278f9b0318
Mada <madalitsomnduwira9@gmail.com>
2023-11-02 17:22:40 +0200

    Commit new

--- app/Http/Controllers/CommunityMemberController.php
+++ app/Http/Controllers/CommunityMemberController.php
@@ -55,7 +55,7 @@ class CommunityMemberController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+
     }
 
     /**
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostsResource;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Mockery\Exception;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index() : AnonymousResourceCollection
     {
-        //
+
+        $posts = Post::with(['user','Post_Comment.user','Post_Likes'])->withCount(['Post_Likes','Post_Comment'])->get();
+
+        return PostsResource::collection($posts);
     }
 
     /**
@@ -34,16 +40,32 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
-        //
+
+        $field  = $request->validate([
+            'description' => 'required',
+            'mode' => 'required'
+        ]);
+
+        $post->update($field);
+
+        return response([
+           'message'=>'updated'
+        ],201);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, Post $post)
     {
-        //
+        try {
+            $post->delete();
+            return response(['message'=>'deleted succuessfuly']);
+        }catch (Exception $e){
+            return response(['message' => $e],500 );
+        }
     }
 }
